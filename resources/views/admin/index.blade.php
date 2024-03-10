b07963280742a73f04b7162b020d46f21dd46446
Amanullah Nilger <nilgeramanullah@gmail.com>
2024-03-10 17:06:35 +0300

    bootsrap,css commit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,13 +7,14 @@
 	<meta name="description-gambolthemes" content="">
 	<meta name="author-gambolthemes" content="">
 	<title>Gambo Supermarket Admin</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<link href="{{asset('admin-css/styles.css')}}" rel="stylesheet">
 	<link href="{{asset('admin-css/admin-style.css')}}" rel="stylesheet">
 	
 	<!-- Vendor Stylesheets -->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-	
+	<link href="{{asset('admin-css/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('admin-css/fontawesome-free/all.min.css')}}" rel="stylesheet">
 </head>
 
     <body class="sb-nav-fixed">
@@ -331,14 +332,14 @@
                 </footer>
             </div>
         </div>
-        <script src="js/jquery-3.4.1.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="vendor/chart/highcharts.js"></script>
-		<script src="vendor/chart/exporting.js"></script>
-		<script src="vendor/chart/export-data.js"></script>
-		<script src="vendor/chart/accessibility.js"></script>
-        <script src="js/scripts.js"></script>
-        <script src="js/chart.js"></script>
+        <script src="{{asset('admin-js/jquery-3.4.1.min.js')}}"></script>
+        <script src="{{asset('admin-js/vendor/bootstrap/bootstrap.bundle.min.js')}}"></script>
+		<script src="{{asset('admin-js/vendor/chart/highcharts.js')}}"></script>
+		<script src="{{asset('admin-js/vendor/chart/exporting.js')}}"></script>
+		<script src="{{asset('admin-js/vendor/chart/export-data.js')}}"></script>
+		<script src="{{asset('admin-js/vendor/chart/accessibility.js')}}"></script>
+        <script src="{{asset('admin-js/scripts.js')}}"></script>
+        <script src="{{asset('admin-js/chart.js')}}"></script>
        
     </body>
 </html>
